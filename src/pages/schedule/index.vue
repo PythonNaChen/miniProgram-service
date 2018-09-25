93a41f658bbd92f25cc6c1b99a3e843edf8ad481
nachen <1078666705@qq.com>
2018-09-25 08:54:54 +0800

    修改日历样式

--- src/pages/schedule/index.vue
+++ src/pages/schedule/index.vue
@@ -23,7 +23,7 @@
             <div class="thead_mask"></div>
             <Calendar @next="next" @prev="prev" multi @select="select" ref="calendar" :tileContent="tileContent"
                       :now="now"/>
-            <div class="unclick_mask"></div>
+            <!--<div class="unclick_mask"></div>-->
             <!-- 提示 -->
             <div class="notice">
                 <div class="notice_box">
@@ -262,7 +262,7 @@
         height: 40px;
         background-color: #ccc;
         position: absolute;
-        top: 33%;
+        top: 148px;
         left: 0;
         opacity: 0.3;
         z-index: 50;
